361acff492698eb0fe43d63ae2ddb57ec48d78de
EledraNguyen <EledraNguyen@6eb003dc-603a-45a0-0b75-6fc3235b7eff>
2010-07-29 05:52:41 +0000

    First base

---
+++ DemoProject/TestAlgo/TestAlgo/Contract/GenericEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace TestAlgo.Library
+{
+    public class GenericEntity : IEntity
+    {
+        public string Name { get; set; }
+        public List<IEntity> Connections { get; set; }
+
+        public GenericEntity()
+        {
+            this.Connections = new List<IEntity>();
+        }
+
+        public GenericEntity(string name)
+        {
+            this.Connections = new List<IEntity>();
+            this.Name = name;
+        }
+    }
+}
---
+++ DemoProject/TestAlgo/TestAlgo/Contract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TestAlgo.Library
+{
+    public interface IEntity
+    {
+        //Name of the entity
+        string Name { get; set; }
+
+        //Set of connected entities with this entity
+        List<IEntity> Connections { get; set; }
+    }
+}
