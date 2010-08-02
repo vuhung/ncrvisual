edab6b5b02450d39094fa6f35b3a62b281d5883a
dangphatpvd@gmail.com <dangphatpvd@gmail.com@6eb003dc-603a-45a0-0b75-6fc3235b7eff>
2010-08-02 17:06:08 +0000

    merge with long's part

---
+++ trunk/DemoProject/TestAlgo/TestAlgo.Web/Uploader.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NCRV;
+
+namespace TestAlgo.Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string ClientBinPath = "";
+        string fileName = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (fuInput.HasFile)
+                try
+                {
+                    if (!fuInput.FileName.Contains(".txt"))
+                    {
+                        lblStatus.Text = "This is not a text file";
+                        return;
+                    }
+                    ClientBinPath = this.Server.MapPath(this.Request.ApplicationPath).Replace("/", "\\") + "ClientBin\\";
+                    fileName = fuInput.FileName;
+                    fuInput.SaveAs(ClientBinPath+
+                         fuInput.FileName);
+                    lblStatus.Text = "File name: " +
+                         fuInput.PostedFile.FileName + "<br>" +
+                         fuInput.PostedFile.ContentLength + " kb<br>" +
+                         "Content type: " +
+                         fuInput.PostedFile.ContentType;
+
+                    Controller controller = new Controller();
+                    controller.SolveData(ClientBinPath, fileName);
+                    lblStatus.Text = "Success, please press the link below to see visualization";
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text = "ERROR: " + ex.Message.ToString();
+                }
+            else
+            {
+                lblStatus.Text = "You have not specified a file.";
+            }
+            
+        }
+    }
+}
