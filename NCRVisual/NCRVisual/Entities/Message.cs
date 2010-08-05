3f27816f8c3cca377b413a04730473ee787c5273
nqk.mail@gmail.com <nqk.mail@gmail.com@6eb003dc-603a-45a0-0b75-6fc3235b7eff>
2010-08-05 03:13:25 +0000

    Add interface required for IDataProvider va them 2 Entity

---
+++ NCRVisual/NCRVisual/Entities/Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace NCRVisual.Entities
+{
+    public class Message
+    {
+        private User sender;
+
+        private String subject;
+
+        private DateTime sentDate;
+
+        public User Sender
+        {
+            get { return sender; }
+            set { sender = value; }
+        }
+
+        public String Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+
+        public DateTime SentDate
+        {
+            get { return sentDate; }
+            set { sentDate = value; }
+        }
+    }
+}
