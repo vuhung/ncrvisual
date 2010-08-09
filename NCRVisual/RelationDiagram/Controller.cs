02d95796c4e777b59426da47213accef6f8bc17d
EledraNguyen <EledraNguyen@6eb003dc-603a-45a0-0b75-6fc3235b7eff>
2010-08-09 07:49:16 +0000

    Add new Project RelationDiagram

---
+++ NCRVisual/RelationDiagram/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+
+namespace NCRVisual.RelationDiagram
+{
+    public class Controller
+    {
+        public string ProjectDirectory { get; set; }
+        public Collection<Point> GetData()
+        {
+            int [][] a=new int[100][];
+            char[] splitArr = { ' ' };
+            int n = 0;
+            ProjectDirectory = "output.txt";
+            StreamReader reader = new StreamReader(ProjectDirectory);
+            int i = 0;
+            while (!reader.EndOfStream)
+            {
+                string s=reader.ReadLine();
+                string[] r = s.Split(' ');
+                n = r.Length;
+                for (int j = 0; j < n; j++)
+                    a[i][j] = Int16.Parse(r[j]) - 49;
+            }
+            return null;
+        }
+        
+
+    }
+}
