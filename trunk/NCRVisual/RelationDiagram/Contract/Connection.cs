64d8069ab8262bfdd87558c1ce4c3d7573af4789
EledraNguyen <EledraNguyen@6eb003dc-603a-45a0-0b75-6fc3235b7eff>
2010-08-18 14:35:36 +0000

    ReUpload missing file - stupid SVN

---
+++ trunk/NCRVisual/RelationDiagram/Contract/Connection.cs
@@ -0,0 +1,29 @@
+﻿namespace NCRVisual.RelationDiagram
+{
+    /// <summary>
+    /// The connection between 2 entities
+    /// </summary>
+    public class Connection
+    {
+        /// <summary>
+        /// Get or set the source entity
+        /// </summary>
+        public IEntity Source { get; set; }
+
+        /// <summary>
+        /// Get or set the destination entity
+        /// </summary>
+        public IEntity Destination { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="e1">The source entity</param>
+        /// <param name="e2">The destination entity</param>
+        public Connection(IEntity e1, IEntity e2)
+        {
+            this.Source = e1;
+            this.Destination = e2;
+        }
+    }
+}
