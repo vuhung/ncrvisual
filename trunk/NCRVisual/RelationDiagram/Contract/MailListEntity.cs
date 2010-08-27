6e7cd5e936e546336adefe95d00bd21f8722b08f
EledraNguyen <EledraNguyen@6eb003dc-603a-45a0-0b75-6fc3235b7eff>
2010-08-27 08:41:19 +0000

    Batch upload after Merging 3 project - Runable

---
+++ trunk/NCRVisual/RelationDiagram/Contract/MailListEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace NCRVisual.RelationDiagram
+{
+    public class MailListEntity : IEntity
+    {
+
+        #region IEntity Members
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public List<IEntity> Connections
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
--- trunk/NCRVisual/RelationDiagram/Controller.cs
+++ trunk/NCRVisual/RelationDiagram/Controller.cs
@@ -4,11 +4,31 @@ using System.IO;
 using System.Windows;
 using System.Net;
 using NCRVisual.RelationDiagram.Algo;
+using System.Xml;
 
 namespace NCRVisual.RelationDiagram
 {
     public class DiagramController
     {
+        #region constant
+        string INPUT_FILE_NAME = "..\\Output\\output.xml";
+        string ID_TAG = "UserId";
+        string NAME_TAG = "Name";
+        string EMAIL_TAG = "Email";
+        string VERTEX_TAG = "Vertex";
+        string EDGE_TAG = "Edge";
+        string START_EDGE_TAG = "Start";
+        string END_EDGE_TAG = "End";
+        string EDGE_VALUE_TAG = "Value";
+            
+
+        #endregion
+
+        #region private
+        private int[][] _input = new int[100][];
+        private int _vertexNumber = 0;
+        #endregion
+
         /// <summary>
         /// Event after reading input from data provider
         /// </summary>
@@ -19,41 +39,31 @@ namespace NCRVisual.RelationDiagram
         /// </summary>
         public IAlgorithm LayoutAlgorithm { get; set; }
 
+        /// <summary>
+        /// Get or set the Entity Collection
+        /// </summary>
+        public Collection<MailListEntity> entityCollection { get; set; }
 
         /// <summary>
         /// Create new instance of Diagram controller
         /// </summary>
         public DiagramController()
         {
-            LayoutAlgorithm = new RectangleAlgorithm();
+            LayoutAlgorithm = new TreeNodeAlgorithm();
+            entityCollection = new Collection<MailListEntity>();
 
             //getfile from output.txt
             WebClient client = new WebClient();
-            client.OpenReadAsync(new Uri("..\\Output\\output.txt", UriKind.Relative));
+            client.OpenReadAsync(new Uri(INPUT_FILE_NAME, UriKind.Relative));
             client.OpenReadCompleted += new OpenReadCompletedEventHandler(client_OpenReadCompleted);
         }
 
         void client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
-        {
-            int[][] _input = new int[100][];
-            int _vertexNumber = 0;
-
-            StreamReader reader = new StreamReader(e.Result);
-            int i = 0;
-            while (!reader.EndOfStream)
+        {            
+            using (XmlReader reader = XmlReader.Create(e.Result))
             {
-                //Read line by line
-                _input[i] = new int[100];
-                string s = reader.ReadLine().Trim();
-                string[] r = s.Split(' ');
-                _vertexNumber = r.Length;
-                for (int j = 0; j < _vertexNumber; j++)
-                {
-                    _input[i][j] = Int16.Parse(r[j]);
-                }
-                i++;
+                ReadRelation(reader);
             }
-            reader.Close();
 
             Collection<Point> tempPoints = LayoutAlgorithm.RunAlgo(_input, _vertexNumber);
 
@@ -61,6 +71,59 @@ namespace NCRVisual.RelationDiagram
             {
                 this.InputReadingComplete(tempPoints, null);
             };
+        }
+
+        void ReadRelation(XmlReader reader)
+        {
+            reader.MoveToContent();
+            int count = 0;
+
+            //Start reading xml
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == VERTEX_TAG)
+                {
+
+                    MailListEntity entity = new MailListEntity();
+
+                    //Read Userid
+                    reader.ReadToFollowing(ID_TAG);
+                    string id = reader.ReadElementContentAsString();
+
+                    //Read Email
+                    reader.ReadToFollowing(EMAIL_TAG);
+                    string email = reader.ReadElementContentAsString();
+
+                    //Read Name
+                    reader.ReadToFollowing(NAME_TAG);
+                    string name = reader.ReadElementContentAsString();
+
+                    entity.UserId = id;
+                    entity.UserId = name;
+                    entity.Email = email;
+                    this.entityCollection.Add(entity);
+
+                    //Build matrix for connection
+                    _input[count] = new int[100];
+
+                    while (reader.ReadToNextSibling(EDGE_TAG))
+                    {
+                        reader.ReadToFollowing(START_EDGE_TAG);
+                        int start = reader.ReadElementContentAsInt();
+                        reader.ReadToFollowing(END_EDGE_TAG);
+                        int end = reader.ReadElementContentAsInt();
+                        reader.ReadToFollowing(EDGE_VALUE_TAG);
+                        int value = reader.ReadElementContentAsInt();
+
+                        _input[start - 1][end - 1] = value;
+                        reader.Read();
+                    }
+
+                    count++;
+                }
+            }
+
+            _vertexNumber = count;
         }
     }
 }
