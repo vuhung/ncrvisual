bbaad9f8c0f49d6298f9f9cb94f1c9331291eae7
nqk.mail <nqk.mail@6eb003dc-603a-45a0-0b75-6fc3235b7eff>
2010-11-01 14:42:17 +0000

    Added the combobox to select year for personal graph

--- trunk/NCRVisual/PersonalInfoAndStatistics/PersonalStatisticsControl.xaml.cs
+++ trunk/NCRVisual/PersonalInfoAndStatistics/PersonalStatisticsControl.xaml.cs
@@ -10,18 +10,12 @@ namespace PersonalInfoAndStatistics
     {
         #region constants
         private const int INVALID_NUM = -1; // Used as default values to indicate a variable hasn't been set
-        private const string MESS_SENT_CAPTION = "Number of messages sent";
-        private const string MESS_RECV_CAPTION = "Number of messages received";
+        private const string MESS_SENT_CAPTION = "Messages sent";
+        private const string MESS_RECV_CAPTION = "Messages received";
         #endregion
 
         #region variable
         // variable for general information
-        /* No need anymore, will decide later to keep or delete
-            private string emailAddr = null;
-            private string name = null;
-            private int messagesSent = INVALID_NUM;
-            private int messagesReceived = INVALID_NUM;
-        */
         // variables used to create the graph
         private List<DateTime> allMessagesSentTime = null;
         private List<DateTime> allMessagesReceivedTime = null;
@@ -31,9 +25,67 @@ namespace PersonalInfoAndStatistics
         private DataPoint[] messagesSentDataPoints = null;
         private Chart messagesRecvChart = null;
         private DataPoint[] messagesRecvDataPoints = null;
+        private bool shouldTrigger = true; // the flag to decide if combo box change event should trigger or not
         #endregion
 
+        private void extractYear(List<DateTime> input)
+        {
+            List<int> years = new List<int>();
+            foreach (object o in comboBox1.Items)
+            {
+                years.Add(Convert.ToInt32(o));
+            }
+            foreach (DateTime dateTime in input)
+            {
+                if (!years.Contains(dateTime.Year))
+                {
+                    years.Add(dateTime.Year);
+                }
+            }
+            years.Sort();
+            comboBox1.Items.Clear();
+            foreach (int i in years)
+            {
+                comboBox1.Items.Add(i);
+            }
+
+            shouldTrigger = false; // change flag so the event won't trigger
+            if (comboBox1.Items[0] != null) // select the first choice
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+            shouldTrigger = true; // restore the flag
+        }
+
         #region getters & setters, but with more functionality (refresh data on update)
+
+        private int Year
+        {
+            get {
+                try
+                {
+                    return Convert.ToInt32(comboBox1.SelectedValue.ToString());
+                }
+                catch (FormatException)
+                {
+                    return -1;
+                }
+                catch (NullReferenceException)
+                {
+                    return -1;
+                }
+
+            }
+            set {
+                // clear the graph current info
+                clearRecvChart();
+                clearSentChart();
+                // refresh the graph
+                processMessageTime(allMessagesSentTime, messagesSentDataPoints);
+                processMessageTime(allMessagesReceivedTime, messagesRecvDataPoints);
+            }
+        }
+
         public string EmailAddr
         {
             get { return labelEmailAddrVal.Content.ToString(); }
@@ -41,7 +93,7 @@ namespace PersonalInfoAndStatistics
                 labelEmailAddrVal.Content = value;
             }
         }
-        public string Name
+        public string MemberName
         {
             get { return labelNameVal.Content.ToString(); }
             set {
@@ -86,6 +138,7 @@ namespace PersonalInfoAndStatistics
             get { return allMessagesSentTime; }
             set {
                 allMessagesSentTime = value; // do the normal task of a setter
+                extractYear(allMessagesSentTime); // get the years availables in the messages
                 // then refresh the graph
                 if (messagesSentChart == null) // make sure it's not null
                 {
@@ -113,21 +166,19 @@ namespace PersonalInfoAndStatistics
                 }
                 else
                 {
-                    foreach (DataPoint tmpDP in messagesSentChart.Series[0].DataPoints)
-                    {
-                        tmpDP.YValue = 0;
-                    }
+                    clearSentChart();
                 }
                 // refresh values of the chart
-                processMessageTime(allMessagesSentTime, messagesSentDataPoints, 2010);
+                processMessageTime(allMessagesSentTime, messagesSentDataPoints);
             }
         }
+
         public List<DateTime> AllMessagesReceivedTime
         {
             get { return allMessagesReceivedTime; }
             set {
                 allMessagesReceivedTime = value; // do the normal task of a setter
-
+                extractYear(allMessagesReceivedTime); // get the years availables in the messages
                 // then refresh the graph
                 if (messagesRecvChart == null) // make sure it's not null
                 {
@@ -156,33 +207,57 @@ namespace PersonalInfoAndStatistics
                 }
                 else
                 {
-                    foreach (DataPoint tmpDP in messagesRecvChart.Series[0].DataPoints)
-                    {
-                        tmpDP.YValue = 0;
-                    }
+                    clearRecvChart();
                 }
                 // refresh values of the chart
-                processMessageTime(allMessagesReceivedTime, messagesRecvDataPoints, 2007);
+                processMessageTime(allMessagesReceivedTime, messagesRecvDataPoints);
             }
         }
         #endregion
 
         #region private functions
-        private void processMessageTime(List<DateTime> dateTimeList, DataPoint[] dataPoints, int year)
+        private void processMessageTime(List<DateTime> dateTimeList, DataPoint[] dataPoints)
         {
             foreach (DateTime tmpDateTime in dateTimeList)
             {
-                //if (tmpDateTime.Year == year)
-                //{
-                    dataPoints[tmpDateTime.Month-1].YValue++;
-                //}
+                if (tmpDateTime.Year == Year)
+                {
+                    dataPoints[tmpDateTime.Month - 1].YValue++;
+                }
             }
         }
+
+            #region functions to clear data
+            private void clearSentChart()
+            {
+                if (messagesSentChart != null)
+                {
+                    foreach (DataPoint tmpDP in messagesSentChart.Series[0].DataPoints)
+                    {
+                        tmpDP.YValue = 0;
+                    }
+                }
+            }
+            private void clearRecvChart()
+            {
+                if (messagesRecvChart != null)
+                {
+                    foreach (DataPoint tmpDP in messagesRecvChart.Series[0].DataPoints)
+                    {
+                        tmpDP.YValue = 0;
+                    }
+                }
+            }
+            #endregion
         #endregion
 
         public PersonalStatisticsControl()
         {
-            InitializeComponent();            
+            InitializeComponent();
+
+            #region populate years to the combo box
+
+            #endregion
 
             #region debug code to create dummy data
             //List<DateTime> tmp1 = new List<DateTime>();
@@ -204,6 +279,19 @@ namespace PersonalInfoAndStatistics
             //AllMessagesReceivedTime = tmp1;
             //AllMessagesSentTime = tmp2;
             #endregion
+
         }
+
+        #region event handler
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox1.SelectedItem != null && shouldTrigger)
+            {
+                this.Year = Convert.ToInt32(comboBox1.SelectedItem);
+            }
+        }
+
+        #endregion
     }
 }
